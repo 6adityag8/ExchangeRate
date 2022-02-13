64847c4b7385c665d33dfc7953b7d4d0f99f30f9
6adityag8 <aditya.vssut@gmail.com>
2022-02-13 13:19:33 +0530

    Refactor for convert endpoint

--- app/api/endpoints/convert.py
+++ app/api/endpoints/convert.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, Path
+from fastapi import APIRouter, HTTPException, status, Path, Query
 
 from app.api.constants import LATEST_PATH_PARAM
 from app.api.utils import request, get_base_currency_dict
@@ -8,9 +8,9 @@ from app.schemas.convert import ConvertedValue
 router = APIRouter()
 
 
-@router.get("/{value}/{base}/{to}", response_model=ConvertedValue)
+@router.get("/{base}/{to}", response_model=ConvertedValue)
 async def convert_currency(
-        value: float = Path(
+        value: float = Query(
             ...,
             title="Value to be Converted",
             description="The value to be converted from base currency to the required currency.",
@@ -42,9 +42,10 @@ async def convert_currency(
     if base not in currencies_dict or to not in currencies_dict:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid country code.",
+            detail="Invalid currency code.",
         )
-    converted_value = (value * currencies_dict.get(to)) / currencies_dict.get(base)
+    base_currency_dict = get_base_currency_dict(base, currencies_dict)
+    converted_value = value * base_currency_dict[to]
     return {
         'base': base,
         'to': to,
