a48ffdc90025cf50eeb25977a7f5e6fa8ed0abd0
6adityag8 <aditya.vssut@gmail.com>
2022-02-13 12:41:48 +0530

    Add validation for convert endpoint

--- app/api/endpoints/convert.py
+++ app/api/endpoints/convert.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Path
 
 from app.api.constants import LATEST_PATH_PARAM
 from app.api.utils import request, get_base_currency_dict
@@ -8,8 +8,30 @@ from app.schemas.convert import ConvertedValue
 router = APIRouter()
 
 
-@router.get("/", response_model=ConvertedValue)
-async def convert_currency(value: float, base: str, to: str):
+@router.get("/{value}/{base}/{to}", response_model=ConvertedValue)
+async def convert_currency(
+        value: float = Path(
+            ...,
+            title="Value to be Converted",
+            description="The value to be converted from base currency to the required currency.",
+            gt=0,
+            example=23.6
+        ),
+        base: str = Path(
+            ...,
+            title="Base Currency Code",
+            description="3-letter ISO currency code of the base currency.",
+            regex="^[A-Z]{3}$",
+            example="EUR"
+        ),
+        to: str = Path(
+            ...,
+            title="Converted Currency Code",
+            description="3-letter ISO currency code of the converted currency.",
+            regex="^[A-Z]{3}$",
+            example="INR"
+        )
+):
     query_parameter = {
         'app_id': settings.OPEN_EXCHANGE_RATE_APP_ID,
         'symbols': '{0},{1}'.format(base, to),
