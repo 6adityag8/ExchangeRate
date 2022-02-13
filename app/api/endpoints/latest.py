e1585efdf8fca5ac7f81763f4dfd1012884669b5
6adityag8 <aditya.vssut@gmail.com>
2022-02-13 13:06:05 +0530

    Add validation for latest endpoint

--- app/api/endpoints/latest.py
+++ app/api/endpoints/latest.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, Set
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Path, Query
+from pydantic import constr
 
 from app.api.constants import LATEST_PATH_PARAM
 from app.api.utils import request, get_base_currency_dict
@@ -10,14 +11,27 @@ from app.schemas.latest import LatestCurrency
 router = APIRouter()
 
 
-@router.get("/", response_model=LatestCurrency)
-async def get_latest_exchange_rate(base: str, currencies: Optional[str] = None):
+@router.get("/{base}", response_model=LatestCurrency)
+async def get_latest_exchange_rate(
+        base: str = Path(
+            ...,
+            title="Base Currency Code",
+            description="3-letter ISO currency code of the base currency.",
+            regex="^[A-Z]{3}$",
+            example="EUR"
+        ),
+        currencies: Optional[Set[constr(regex="^[A-Z]{3}$")]] = Query(
+            None,
+            title="Limit results to specific currencies",
+            description="List of 3-letter ISO currency codes for which the results are needed to be limited to."
+        )
+):
     query_parameter = {
         'app_id': settings.OPEN_EXCHANGE_RATE_APP_ID,
         'prettyprint': False
     }
     if currencies:
-        query_parameter['symbols'] = '{0},{1}'.format(base, currencies)
+        query_parameter['symbols'] = '{0},{1}'.format(base, ','.join(currencies))
     response = await request(path_param=LATEST_PATH_PARAM, query_param=query_parameter)
     currencies_dict = response.json().get('rates', {})
     if base not in currencies_dict:
