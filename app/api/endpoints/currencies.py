965b1d38bbabb8a84e051d8973b0f6136df0733b
6adityag8 <aditya.vssut@gmail.com>
2022-02-13 10:48:31 +0530

    Refactor currencies endpoint

--- app/api/endpoints/currencies.py
+++ app/api/endpoints/currencies.py
@@ -7,18 +7,24 @@ from app.schemas.currencies import CurrencyCode
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=CurrencyCode)
 async def get_all_currencies():
     response = await request(path_param=CURRENCIES_PATH_PARAM)
-    return response.json()
+    return {
+        'codes': response.json()
+    }
 
 
 @router.get("/{currency_code}", response_model=CurrencyCode)
 async def get_currency_from_currency_code(currency_code: str):
     response = await get_all_currencies()
-    if currency_code not in response:
+    if currency_code not in response['codes']:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid country code.",
         )
-    return {currency_code: response[currency_code]}
+    return {
+        'codes': {
+            currency_code: response['codes'][currency_code]
+        }
+    }
--- app/schemas/currencies.py
+++ app/schemas/currencies.py
@@ -1,5 +1,7 @@
+from typing import Dict
+
 from pydantic import BaseModel
 
 
 class CurrencyCode(BaseModel):
-    currency_code: str
+    codes: Dict[str, str]
