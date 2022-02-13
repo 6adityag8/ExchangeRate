ae64dcf24594bb8dbb1c03218b5e5566dc9d7b51
6adityag8 <aditya.vssut@gmail.com>
2022-02-13 09:27:08 +0530

    Add convert endpoint's response schema

--- app/api/endpoints/convert.py
+++ app/api/endpoints/convert.py
@@ -9,17 +9,23 @@ router = APIRouter()
 
 
 @router.get("/", response_model=ConvertedValue)
-async def convert_currency(value: float, from_currency: str, to_currency: str):
+async def convert_currency(value: float, base: str, to: str):
     query_parameter = {
         'app_id': settings.OPEN_EXCHANGE_RATE_APP_ID,
-        'symbols': '{0},{1}'.format(from_currency, to_currency)
+        'symbols': '{0},{1}'.format(base, to),
+        'prettyprint': False
     }
     response = await request(path_param=LATEST_PATH_PARAM, query_param=query_parameter)
     currencies_dict = response.json().get('rates', {})
-    if from_currency not in currencies_dict or to_currency not in currencies_dict:
+    if base not in currencies_dict or to not in currencies_dict:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid country code.",
         )
-    converted_value = (value * currencies_dict.get(to_currency)) / currencies_dict.get(from_currency)
-    return {'converted_value': converted_value}
+    converted_value = (value * currencies_dict.get(to)) / currencies_dict.get(base)
+    return {
+        'base': base,
+        'to': to,
+        'base_value': value,
+        'converted_value': converted_value
+    }
--- app/schemas/convert.py
+++ app/schemas/convert.py
@@ -2,4 +2,7 @@ from pydantic import BaseModel
 
 
 class ConvertedValue(BaseModel):
+    base: str
+    to: str
+    base_value: str
     converted_value: str
